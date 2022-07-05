056c8339308cbdfeb3149a4e8b31a648291a5315
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-07-06 04:14:33 +0800

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Module for a script that adds all arguments to a Python list"""
-import json
+"""Script continually adds arguments to a JSON string in a file
+    called add_item.json
+"""
+
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-file_name = "add_item.json"
-
-# can also use os.path.exists(file) for try and except
-# be sure to import os.path
-try:
-    json_list = load_from_json_file(file_name)
-except:
-    json_list = []
-for item in argv[1:]:
-    json_list.append(item)
-
-save_to_json_file(json_list, file_name)
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file("add_item.json")
+    
+    except Exception:
+        json_list = []
+    
+    i = 1
+    while (i < len(argv)):
+        json_list.append(argv[i])
+        i += 1
+    save_to_json_file(json_list, "add_item.json")
